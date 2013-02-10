775c54df1026e9e3d3435e2f705d60c41bf0fd6e
myfreeweb <floatboth@me.com>
2013-02-10 19:33:47 +0400

    fix host

--- src/swaggerator/host.clj
+++ src/swaggerator/host.clj
@@ -4,5 +4,10 @@
 
 (defn wrap-host-bind [handler]
   (fn [req]
-    (binding [*host* (str (-> req :scheme name) "://" (-> req :remote-addr))]
+    (binding [*host* (str (-> req :scheme name)
+                          "://"
+                          (or (get-in req [:headers "host"])
+                              (-> req :server-name))
+                          (when (not= (-> req :server-port) 80)
+                            (str ":" (-> req :server-port))))]
       (handler req))))
