786e4326b4796d819afe2373079ca51f3ab3cd7b
myfreeweb <floatboth@me.com>
2013-02-16 23:00:53 +0400

    update validation library, use it more efficiently

--- src/swaggerator/validator.clj
+++ src/swaggerator/validator.clj
@@ -1,6 +1,7 @@
 (ns swaggerator.validator
   (:import [com.github.fge.jsonschema.main JsonSchema JsonSchemaFactory]
            [com.github.fge.jsonschema.util JsonLoader]
+           [com.github.fge.jsonschema.report ValidationReport]
            [com.fasterxml.jackson.core JsonFactory]
            [com.fasterxml.jackson.databind JsonNode ObjectMapper]
            [java.io StringWriter])
@@ -19,11 +20,16 @@
 (defn make-validator [schema]
   (let [so (make-schema-object schema)]
     (fn [x]
-      (let [root (.asJsonObject (.validate so (clojure->jsonnode x)))
-            sw (StringWriter.)
-            jgen (.createJsonGenerator (or factory/*json-factory* factory/json-factory) sw)]
-        (.writeTree ^ObjectMapper mapper jgen root)
-        (.toString sw)))))
+      (.validate so (clojure->jsonnode x)))))
+
+(defn is-success? [^ValidationReport report]
+  (.isSuccess report))
+
+(defn to-json [^ValidationReport report]
+  (let [sw (StringWriter.)
+        jgen (.createJsonGenerator (or factory/*json-factory* factory/json-factory) sw)]
+    (.writeTree ^ObjectMapper mapper jgen (.asJsonObject report))
+    (.toString sw)))
 
 (defn wrap-json-schema-validator
   "Ring middleware that validates any POST/PUT requests
@@ -32,10 +38,10 @@
   (let [v (make-validator schema)]
     (fn [req]
       (if (#{:post :put :patch} (-> req :request-method))
-        (let [results (-> req :non-query-params v)]
-          (if (= results "{}")
+        (let [result (-> req :non-query-params v)]
+          (if (is-success? result)
             (handler req)
             {:status 422
              :headers {"Content-Type" "application/json;charset=utf-8"}
-             :body results}))
+             :body (to-json result)}))
         (handler req)))))
