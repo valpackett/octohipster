ea7e84744df7b93b29c34f1219c3381cbf5b3779
myfreeweb <floatboth@me.com>
2013-03-19 16:15:56 +0400

    1.5 only

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/myfreeweb/swaggerator"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.reader "0.7.0"]
                  [ring/ring-core "1.1.8"]
                  [compojure "1.1.5"]
@@ -14,8 +14,7 @@
                  [inflections "0.8.0"]
                  [com.github.fge/json-schema-validator "1.99.9"]
                  [com.damnhandy/handy-uri-templates "1.1.7"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :dev {:dependencies [[speclj "2.5.0"                 :exclusions [org.clojure/clojure]]
+  :profiles {:dev {:dependencies [[speclj "2.5.0"                 :exclusions [org.clojure/clojure]]
                                   [com.novemberain/monger "1.4.2" :exclusions [org.clojure/clojure]]
                                   [http-kit "2.0.0-RC4"           :exclusions [org.clojure/clojure]]
                                   [ring-ratelimit "0.1.0"         :exclusions [org.clojure/clojure]]
