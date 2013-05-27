a7a1bf3baad64a1bc7d019fe4fa2f0f485ff82d7
myfreeweb <floatboth@me.com>
2013-05-27 14:21:33 +0400

    deps

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.0-beta2"]
+                 [ring/ring-core "1.2.0-beta3"]
                  [ring.middleware.jsonp "0.1.2"]
                  [liberator "0.8.0"]
                  [clout "1.1.0"]
-                 [cheshire "5.1.1"]
+                 [cheshire "5.2.0"]
                  [clj-yaml "0.4.0"]
-                 [inflections "0.8.0"]
+                 [inflections "0.8.1"]
                  [org.bovinegenius/exploding-fish "0.3.3"]
                  [com.github.fge/json-schema-validator "2.1.3"]
                  [com.damnhandy/handy-uri-templates "1.1.7"]]
-  :profiles {:dev {:dependencies [[speclj "2.6.1"                 :exclusions [org.clojure/clojure]]
+  :profiles {:dev {:dependencies [[speclj "2.7.2"                 :exclusions [org.clojure/clojure]]
                                   [com.novemberain/monger "1.4.2" :exclusions [org.clojure/clojure]]
-                                  [http-kit "2.0.1"               :exclusions [org.clojure/clojure]]
+                                  [http-kit "2.1.1"               :exclusions [org.clojure/clojure]]
                                   [ring-mock "0.1.3"              :exclusions [org.clojure/clojure]]]}}
-  :plugins [[speclj "2.6.1"]
+  :plugins [[speclj "2.7.2"]
             [codox "0.6.4"]
             [lein-release "1.0.0"]]
   :bootclasspath true
